7aa74fa0c404ee4a8867bfb99f717b9bedab1641
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-03-27 14:43:20 -0300

    tests with nodejs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 // Controllers
 const OngController = require('./controllers/OngController')
@@ -13,14 +14,37 @@ routes.post('/sessions', SessionController.create)
 
 // Ongs Routes
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY] : Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}),OngController.create)
 
 // Incidents Routes
-routes.get('/incidents', IncidentController.index)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}),IncidentController.index)
+
 routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required() 
+    })
+}),IncidentController.delete)
 
 // Profile Routes
-routes.get('/profile', ProfileController.index)
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}),ProfileController.index)
 
 module.exports = routes
